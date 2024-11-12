08141e6d234118dbbadb4b0f499ebfc6b7063788
YoussefSewalm <eng.yousefsewalem@gmail.com>
2024-11-12 18:50:18 +0200

    Update ReadDataFromExcel.java

--- src/test/java/org/sample/TestingEcommerce/ReadDataFromExcel.java
+++ src/test/java/org/sample/TestingEcommerce/ReadDataFromExcel.java
@@ -14,33 +14,19 @@ public class ReadDataFromExcel {
 
 	public String[][] ReadRegData() throws InvalidFormatException, IOException, org.apache.poi.openxml4j.exceptions.InvalidFormatException
 	{
-//First Step: b2olo e3mly open l file l excel 2ly ana 3malto w maleh b data
 		File file_1=new File("C:\\Users\\giga\\Desktop\\DataDriven.xlsx");
-//Second step: b2olo eft7ly l Workbook bta3 l excel dh 3shan a3rf 3any kam sheets w details l file 3shan mno
-//h3ml access ll sheets wl rows
-		XSSFWorkbook Workbook_1=new XSSFWorkbook(file_1);
-//Third Step: h3ml access b2a ll sheet 2ly 3ayzha w edeto esm l Page mn excel 2ly ana 3amlo "RegisterPage" 		
+		XSSFWorkbook Workbook_1=new XSSFWorkbook(file_1);		
 		XSSFSheet RegDataSheet=Workbook_1.getSheet("RegisterPage");
-//Fourth Step: haro7 ashof 3andy kam Row fl sheet dh "RegisterPage"::Remember awl row 5als 3andy mogarad titles
-//Msh Data f 3shan keda hashel l row dh lma agy ab3at w hnshof l klam dh
-		int number_of_rows=RegDataSheet.getPhysicalNumberOfRows();
-//Fifth Step: Haro7 ashof 3andy kam Column fl Sheet dh 		
+		int number_of_rows=RegDataSheet.getPhysicalNumberOfRows();	
 		int number_of_columns=RegDataSheet.getRow(0).getLastCellNum();
-//Sixth Step: hbd2 a3ml l array 2ly hystlm l data deh mn l Excel..hla2y hena 3mlt rows-1 w dh 3shan zy m2olt
-// awl Row 3andy dh Titles msh Data 3shan keda msh bb3to
-		String[][]reg_data=new String[number_of_rows-1][number_of_columns];
-//Final Step hbd2 a3ml loop b2a fl sheet kolo w a7ot l data fl array		
-		for(int i=1;i<number_of_rows;i++)//bda2t i=1 msh b 0 l2n l func bta3t GetRow 2ly ta7t bta5od l i 
-			//w ana fl excel awl row 3andy 1 msh 0 f 3`asb 3any lazm abd2 l i b 1
-			//f 3shan keda zbatha reg_data[i-1][j] 3shan awl element m3ya yb2a reg_data[0][0]
+		String[][]reg_data=new String[number_of_rows-1][number_of_columns];	
+		for(int i=1;i<number_of_rows;i++)
 		{
 			for(int j=0;j<number_of_columns;j++)
 			{
 				//
-				XSSFRow Row=RegDataSheet.getRow(i);//3malt access ll rows mn l sheet 2ly ana 3mlto
-				reg_data[i-1][j]=Row.getCell(j).toString();//5adt l data b3d m gebt row mo3yna w gebt cell mo3yna feh
-			// W lazem a7wl l data bta3ty l String [nafs l no3 2ly l data m7tagah 2ly hwa ana kont hb3tha 
-				//manual 3ady h5leha nafs l data type deh
+				XSSFRow Row=RegDataSheet.getRow(i);
+				reg_data[i-1][j]=Row.getCell(j).toString();
 			}
 		}	
 		return reg_data;
